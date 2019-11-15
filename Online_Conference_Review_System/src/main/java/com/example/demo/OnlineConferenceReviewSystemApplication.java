6c11a078dd11af862e68b7a4b2cfec9a27cfeafe
Unknown <michael.lin2032@gmail.com>
2019-11-15 15:09:15 -0600

    Created final project

---
+++ Online_Conference_Review_System/src/main/java/com/example/demo/OnlineConferenceReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineConferenceReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineConferenceReviewSystemApplication.class, args);
+	}
+
+}
