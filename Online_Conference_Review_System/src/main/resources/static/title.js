0d7d96001f855e964b1d0a0d981bc214f0b1d738
Unknown <michael.lin2032@gmail.com>
2019-11-18 00:31:35 -0600

    outline of register/login done

--- Online_Conference_Review_System/src/main/resources/static/title.js
+++ Online_Conference_Review_System/src/main/resources/static/title.js
@@ -3,6 +3,32 @@ function removeButtonRegister() {
 	 elem.parentNode.removeChild(elem);
 	 elem = document.getElementById('Login');
 	 elem.parentNode.removeChild(elem);
+	 elem = document.getElementById('title');
+	 elem.parentNode.removeChild(elem);
+	 elem = document.getElementById('description');
+	 elem.parentNode.removeChild(elem);
+	 elem = document.getElementById('register_form1');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form2');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form3');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form4');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form5');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form6');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form7');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('register_form8');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('role');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('Rsubmit');
+	 elem.style.visibility = 'visible';
+	 elem = document.getElementById('Rfname');
+	 elem.readOnly = false;
 	}
 	
 function removeButtonLogin() {
@@ -10,10 +36,16 @@ function removeButtonLogin() {
 	 elem.parentNode.removeChild(elem);
 	 elem = document.getElementById('Login');
 	 elem.parentNode.removeChild(elem);
+	 elem = document.getElementById('title');
+	 elem.parentNode.removeChild(elem);
+	 elem = document.getElementById('description');
+	 elem.parentNode.removeChild(elem);
 	 elem = document.getElementById('Login_form');
 	 elem.style.visibility = 'visible';
-	 elem = document.getElementById('email');
+	 elem = document.getElementById('Lemail');
 	 elem.readOnly = false;
-	 elem = document.getElementById('Password');
+	 elem = document.getElementById('LPassword');
 	 elem.readOnly = false;
+	 elem = document.getElementById('login');
+	 elem.disabled = false;
 	}
