4bbef00f58a404529138d5e483ab772c25a6a809
Unknown <michael.lin2032@gmail.com>
2019-11-20 08:54:31 -0600

    added class demo

---
+++ demo/src/main/java/com/example/demo/domain/Employee.java
@@ -0,0 +1,62 @@
+package com.example.demo.domain;
+
+public class Employee {
+	
+	private String fname;
+	private String lname;
+	private String ssn;
+	
+	
+	
+	
+	/**
+	 * @param fname
+	 * @param lname
+	 * @param ssn
+	 */
+	public Employee(String fname, String lname, String ssn) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.ssn = ssn;
+	}
+	/**
+	 * @return the fname
+	 */
+	public String getFname() {
+		return fname;
+	}
+	/**
+	 * @param fname the fname to set
+	 */
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	/**
+	 * @return the lname
+	 */
+	public String getLname() {
+		return lname;
+	}
+	/**
+	 * @param lname the lname to set
+	 */
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	/**
+	 * @return the ssn
+	 */
+	public String getSsn() {
+		return ssn;
+	}
+	/**
+	 * @param ssn the ssn to set
+	 */
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	
+	
+}
