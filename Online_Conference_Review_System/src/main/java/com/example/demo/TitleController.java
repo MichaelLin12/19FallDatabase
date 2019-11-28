e2958cbd3a5423cc490aaec928d54b18cb6c92c4
Unknown <michael.lin2032@gmail.com>
2019-11-27 22:43:53 -0600

    changed to template folder

--- Online_Conference_Review_System/src/main/java/com/example/demo/TitleController.java
+++ Online_Conference_Review_System/src/main/java/com/example/demo/TitleController.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,8 +11,7 @@ public class TitleController {
 
 	private static final Logger logger = LoggerFactory.getLogger(TitleController.class);
 	
-	
-	@RequestMapping(value="/title", method = RequestMethod.GET)
+	@RequestMapping(value="/title",method= RequestMethod.GET)
 	public String title()
 	{
 		return "conference_view";
---
+++ Online_Conference_Review_System/src/main/resources/static/dashboard.js
@@ -0,0 +1,9 @@
+<script>
+function nextButton(name) {
+	  var dashBoard_Select = name;
+	  if dashBoard_Select == null {
+	    greeting = "Good morning";
+	  }
+    document.getElementById("nextButton").style.color = "red";
+}
+</script>
