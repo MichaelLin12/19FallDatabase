3c952e56f4391aef8482eafa235fd53a364fff58
Unknown <michael.lin2032@gmail.com>
2019-12-04 23:22:24 -0600

    added some files

---
+++ Online_Conference_Review_System/src/main/java/com/example/controller/ReviewController.java
@@ -0,0 +1,40 @@
+package com.example.controller;
+
+import com.example.service.ReviewRepository;
+import com.example.entity.ReviewTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ReviewController {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @PostMapping("/addreview")
+    public String addReview(@RequestParam(name = "email") String email,
+                            @RequestParam(name = "paperid") long paperid,
+                            @RequestParam(name = "techmerit") int techmerit,
+                            @RequestParam(name = "readability") int readability,
+                            @RequestParam(name = "originality") int originality,
+                            @RequestParam(name = "relevance") int relevance,
+                            @RequestParam(name = "recomm") int recomm,
+                            @RequestParam(name = "committeecomment") String committeecomment,
+                            @RequestParam(name = "authorcomment") String authorcomment,
+                            Model model
+    ) {
+        try {
+            ReviewTable reviewTable = new ReviewTable(email, paperid, techmerit, readability, originality, relevance,
+                    recomm, committeecomment, authorcomment);
+            reviewRepository.create(reviewTable);
+            return "reviewsuccess";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "error";
+        }
+
+    }
+}
