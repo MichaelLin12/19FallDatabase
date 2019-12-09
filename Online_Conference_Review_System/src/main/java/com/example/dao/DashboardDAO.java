e880b237a6de7bfeeb5e471154ff782ba5c6e565
anantnatekar <40249369+anantnatekar@users.noreply.github.com>
2019-12-09 00:30:19 -0600

    Add files via upload

---
+++ Online_Conference_Review_System/src/main/java/com/example/dao/DashboardDAO.java
@@ -0,0 +1,29 @@
+package com.example.demo.dao;
+
+import java.awt.print.Paper;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.dao.domain.CompanyMapper;
+import com.example.demo.domain.Dashboard;
+import com.example.demo.domain.Employee;
+
+
+@Component
+public class DashboardDAO {
+	@Autowired
+	private CompanyMapper companyMapper;
+	
+	public List<Dashboard> getdashboardList() {
+		
+		List<Dashboard> paperList = new ArrayList<Dashboard>();
+		
+		paperList = companyMapper.getPaperList();
+		
+		return paperList;
+	}
+	
+}
